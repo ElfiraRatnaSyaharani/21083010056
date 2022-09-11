49e5f82dd6e764d0f6e8e707e8f9253a27477e8f
ElfiraRatnaSyaharani <21083010056@student.upnjatim.ac.id>
2022-09-11 09:25:19 +0700

    first commit

---
+++ Tugas1.sh
@@ -0,0 +1,50 @@
+pwd
+
+ls
+
+ls -l
+
+cd "Belajar Bash Dasar"
+cd
+
+cat tugas1_elfira.py
+
+nano tugas_sisop.txt
+
+mkdir 'SikopA'
+
+cd Pictures
+rm tugas_sisop.txt
+
+rmdir sistop
+
+rm -rf SikopA
+
+mv tugas.txt SikopA
+
+cp tugas_sisop.txt Pictures
+
+df
+
+top
+
+free
+
+python3
+
+pix --version
+
+whereis pix
+
+which pix
+
+whatis pix
+
+locate tugas1_elfira.py
+
+find tugas1_elfira.py
+
+ping siamik.upnjatim.ac.id
+
+sudo apt install nodejs
+
