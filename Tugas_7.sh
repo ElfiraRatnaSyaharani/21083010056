d64f730d51b393a79f01080bc4ef530cc0165706
ElfiraRatnaSyaharani <21083010056@student.upnjatim.ac.id>
2022-11-02 17:24:23 +0700

    Tugas 7

---
+++ Tugas_7.sh
@@ -0,0 +1,10 @@
+lbpersegi() {
+         echo "Masukkan Panjang : "
+         read p
+         echo "Masukkan Lebar : "
+         read l
+         let luas=$p*$l
+         echo -e "Luas Persegi : \n$luas"
+}
+
+lbpersegi
